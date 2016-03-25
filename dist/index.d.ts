070adb6a0dc38a39bb3d26184ffc631eea3d4e84
Carl Ansley <carl.ansley@gmail.com>
2016-03-25 13:47:03 -0400

    Upgrade to 0.0.3, fixed export issue

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,4 @@
-import * as swagger from 'swagger2';
-declare var _default: {
-    ui: (document: swagger.Document) => (context: any, next: () => Promise<void>) => Promise<void>;
-    validate: (document: swagger.Document) => (context: any, next: () => Promise<void>) => Promise<void>;
-};
-export default _default;
+import koaValidate from './validate';
+import koaUi from './ui';
+export declare const validate: typeof koaValidate;
+export declare const ui: typeof koaUi;
--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,6 @@
 /* tslint:enable:no-unused-variable */
 const validate_1 = require('./validate');
 const ui_1 = require('./ui');
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    ui: ui_1.default,
-    validate: validate_1.default
-};
+exports.validate = validate_1.default;
+exports.ui = ui_1.default;
 //# sourceMappingURL=index.js.map
