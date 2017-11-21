8bbb2c98eccf7da039594732940283994c738521
Brad Jones <brad@bjc.id.au>
2017-11-21 12:47:52 +1100

    Rebuild the dist

--- dist/validate.js
+++ dist/validate.js
@@ -1,5 +1,5 @@
-// validate.ts
 "use strict";
+// validate.ts
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -9,8 +9,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
-    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -88,7 +88,7 @@ function default_1(document) {
                         context.status = 404;
                         return [2 /*return*/];
                     }
-                    validationErrors = swagger.validateRequest(compiledPath, context.method, context.request.query, context.request.body);
+                    validationErrors = swagger.validateRequest(compiledPath, context.params, context.method, context.request.query, context.request.body);
                     if (validationErrors === undefined) {
                         // operation not defined, return 405 (method not allowed)
                         if (context.method !== 'OPTIONS') {
