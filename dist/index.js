b5ea7e7b2fa8f80c68436bdf4ff395f2c9d00113
Carl Ansley <carl.ansley@gmail.com>
2018-02-06 16:49:05 -0500

    Upgrade to 0.0.40

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /*
  The MIT License
 
- Copyright (c) 2014-2017 Carl Ansley
+ Copyright (c) 2014-2018 Carl Ansley
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
